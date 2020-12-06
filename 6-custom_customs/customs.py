81cc45b894860009a32cc882ae1ed66834c86abd
alex <alex@pop-os.localdomain>
2020-12-05 22:24:51 -0700

    day 6 part 1

--- 6-custom_customs/customs.py
+++ 6-custom_customs/customs.py
@@ -18,10 +18,20 @@ def count_unique_answers(list_of_answers):
         )
     )
 
+def count_common_answers(list_of_answers):
+    return len(set(list_of_answers[0]).intersection(*list_of_answers))
+
 def answer_sum_counts(answers):
     return sum(count_unique_answers(a) for a in answers)
+
+def answer_sum_common_counts(answers):
+    return sum(count_common_answers(a) for a in answers)
 
 if __name__ == "__main__":
     # part 1
     answers = read_customs_file('answers.txt')
     print('sum of answers: ' + str(answer_sum_counts(answers)))
+
+    # part 2
+
+    print('sum of common answers: ' + str(answer_sum_common_counts(answers)))
--- 6-custom_customs/test_customs.py
+++ 6-custom_customs/test_customs.py
@@ -15,7 +15,16 @@ def test_counting_unique_answers():
     assert customs.count_unique_answers(["a"]) == 1
     assert customs.count_unique_answers(["a", "b"]) == 2
     assert customs.count_unique_answers(["a", "b", "a"]) == 2
+    
+def test_counting_common_answers():
+    assert customs.count_common_answers(["a"]) == 1
+    assert customs.count_common_answers(["a", "b"]) == 0
+    assert customs.count_common_answers(["a", "ab", "a",]) == 1
 
 def test_summing_answer_counts():
     answers = customs.read_customs_file('example_answers.txt')
     assert customs.answer_sum_counts(answers) == 11
+
+def test_summing_common_answer_counts():
+    answers = customs.read_customs_file('example_answers.txt')
+    assert customs.answer_sum_common_counts(answers) == 6
