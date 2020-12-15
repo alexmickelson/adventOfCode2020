bd0bba0673c04f5d681c924467e61d4d28ec304f
Alex Mickelson <alexmickelson96@gmail.com>
2020-12-14 23:27:06 -0700

    day 15 cleaned up

--- 15/memory.py
+++ 15/memory.py
@@ -10,33 +10,6 @@ def get_next_number(numbers, previous_number, previous_number_count):
     last_occurence_count = numbers.get(previous_number, previous_number_count)
     return previous_number_count - last_occurence_count
 
-# def extend_list_to(previous_numbers, target_number):
-#     # if len(previous_numbers) == target_number:
-#     #     return previous_numbers
-#     # else:
-#     #     next_number = get_next_number(previous_numbers)
-#     #     previous_numbers.append(next_number)
-#     #     return extend_list_to(previous_numbers, target_number)
-#     for i in range(target_number):
-#         if i >= len(previous_numbers):
-#             next = get_next_number(previous_numbers)
-#             previous_numbers.append(next)
-#         if i % 1000 == 0:
-#             print(i)
-#     return previous_numbers
-
-
-# def _get_number_at_count(previous_numbers, last_number, target_index):
-    # if(current_index % 1000 == 0):
-    #     print(current_index)
-    # if target_index == current_index:
-    #     return get_next_number(previous_numbers, last_number, current_index)
-    # else:
-    #     current_number = get_next_number(previous_numbers, last_number)
-    #     _i_two_before, i_one_before = previous_numbers.get(current_number, current_index)
-    #     previous_numbers[current_number] = (i_one_before, current_index)
-    #     return _get_number_at_index(previous_numbers, numbers_left - 1)
-
 
 def get_number_at_count(count, previous_numbers):
     numbers = {number: i+1 for i, number in enumerate(previous_numbers)}
--- 15/test_memory.py
+++ 15/test_memory.py
@@ -1,38 +1,6 @@
 from typing_extensions import get_args
 import memory
 
-# def test_get_next_number():
-#     starting_numbers = memory.read_starting_numbers('example.txt')
-#     actual_next_number  = memory.get_next_number(starting_numbers)
-#     assert actual_next_number == 0
-#     starting_numbers.append(actual_next_number)
-#     actual_second_number = memory.get_next_number(starting_numbers)
-#     assert actual_second_number == 1
-
-# def test_get_next_number():
-#     starting_numbers = memory.read_starting_numbers('example2.txt')
-#     actual_next_number  = memory.get_next_number(starting_numbers)
-#     assert actual_next_number == 0
-#     starting_numbers.append(actual_next_number)
-#     actual_second_number = memory.get_next_number(starting_numbers)
-#     assert actual_second_number == 3
-#     starting_numbers.append(actual_second_number)
-#     actual_third_number = memory.get_next_number(starting_numbers)
-#     assert actual_third_number == 3
-
-# def test_extend_list_to():
-#     starting_numbers = [0, 3, 6]
-#     actual_numbers = memory.extend_list_to(starting_numbers, 11)
-#     expected_numbers = [0, 3, 6, 0, 3, 3, 1, 0, 4, 0, 2]
-#     assert actual_numbers == expected_numbers
-
-# def test_extend_list_to():
-#     starting_numbers = [1, 3, 2]
-#     actual_numbers = memory.extend_list_to(starting_numbers, 11)
-#     expected_numbers = [1, 3, 2, 0, 0, 1, 5, 0, 3, 7, 0]
-#     assert actual_numbers == expected_numbers
-
-
 def test_2020th_number():
     starting_numbers = [1, 3, 2]
     actual_number = memory.get_number_at_count(4, starting_numbers)
